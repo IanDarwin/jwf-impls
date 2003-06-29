3a8d6a7339c87cf916d4a5ffda6345292229cae2
Ian Darwin <ian@darwinsys.com>
2003-06-29 16:16:35 +0000

    build.xml: copy: add copy of WEB-INF/lib/*.jar. src/metadata/web.xml: add Servlet definition and mapping for servletonly; 	add resource-ref for JDBC datasource. src/servletonly/index.html: simplify name of servlet. Added dbcp.jar and pool.jar for JDBC datasource. src/servletonly/ServletOnly.java : initial checkin of worst-case Servlet impl.

---
+++ jwf.servletonly/src/servletonly/ServletOnly.java
@@ -0,0 +1,120 @@
+package javawebframeworks;
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.naming.*;
+
+/*
+ * Servlet-only implementation for the JavaWebFramework project
+ * @author  Ian F. Darwin
+ * @version $Id$
+ */
+public class ServletOnly extends HttpServlet
+{
+	private Connection con;
+
+	private PreparedStatement st;
+
+	/** Called in response to a GET request (data encoded in the URL) */
+	public void init() throws ServletException {
+		try {
+			Context ctx = new InitialContext();
+
+			String dsn = "java:comp/env/jdbc/javawebframeworks";
+			System.out.println("Looking up " + dsn);
+			Object o = ctx.lookup(dsn);
+			javax.sql.DataSource d = (javax.sql.DataSource)o;
+
+			System.out.println("Getting connection ");
+			con = d.getConnection();
+			System.out.println("Got it!");
+
+			st = con.prepareStatement(
+				"insert into people (firstName, lastName, address1, address2," +
+				"city, province, zipcode, country)" +
+				"values(?,?,?,?,?,?,?,?)");
+
+		} catch (NamingException ex) {
+			ex.printStackTrace();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/** Called in response to a POST request (data unencoded on the socket) */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+
+		ServletContext application = getServletContext();
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter(); 
+
+		// BOILERPLATE beginning
+		out.println("<!DOCTYPE html PUBLIC " +
+			"\"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" +
+			"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"\n" +
+			">");
+
+		out.println("<html>");
+		out.println("<head><title>Servlet Output</title></head>");
+		out.println("<body>");
+
+		// Servlet logic code and main HTML goes here.
+		if (con == null) {
+			out.println("<h2>Error</h2>");
+			out.println("<p>Database connection not made. Aborting.</p>");
+			return;
+		}
+		out.println("Debug: DB connection = " + con);
+
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String address1 = request.getParameter("address1");
+		String address2 = request.getParameter("address2");
+		String city = request.getParameter("city");
+		String province = request.getParameter("province");
+		String postcode = request.getParameter("postcode");
+		String country = request.getParameter("country");
+
+		int ret = 0;
+		try {
+			int i = 1;
+			st.setString(i++, firstName);
+			st.setString(i++, lastName);
+			st.setString(i++, email);
+			st.setString(i++, address1);
+			st.setString(i++, address2);
+			st.setString(i++, city);
+			st.setString(i++, province);
+			st.setString(i++, postcode);
+			st.setString(i++, country);
+
+			ret = st.executeUpdate();
+		} catch (SQLException ex) {
+			out.println("<h1>Error</h1>");
+			out.println("<pre>");
+			ex.printStackTrace(out);
+			out.println("</pre>");
+		}
+
+		if (ret != 1) {
+			out.println("<h2>Error</h2>");
+			out.println("Unable to insert into database, ret = " + ret);
+			return;
+		}
+
+		out.println("<h1>Thank you!</h1>");
+		out.println("Thank you for registering with us!");
+		out.println("If this were a real service, you would now be");
+		out.println("receiving a confirming email with more instructions.");
+
+		// BOILERPLATE ending
+		out.println("</body>");
+		out.println("</html>");
+		out.flush();
+	}
+}
