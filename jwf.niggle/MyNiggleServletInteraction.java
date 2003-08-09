012d11594ef3a3c1b9f98529dbff3b91a0281c55
Ian Darwin <ian@darwinsys.com>
2003-08-09 17:14:16 +0000

    Fetch DataSource before Record; renamed DS to peopledb and record to people to match table in DB.

--- jwf.niggle/MyNiggleServletInteraction.java
+++ jwf.niggle/MyNiggleServletInteraction.java
@@ -2,15 +2,14 @@ import org.niggle.servlet.NiggleConfig;
 import org.niggle.servlet.ServletInteraction;
 import org.oreodata.OreoDataSource;
 import org.oreodata.Record;
+
 import javax.servlet.http.*;
 import java.io.IOException;
 
 /** The "action" class for the Person Entry implementation.
  */
 public class MyNiggleServletInteraction extends ServletInteraction {
-    /**
-     * Construct a ServletInteraction; just delegate to super.
-     */
+    /** Construct a ServletInteraction; just delegate to super. */
     public MyNiggleServletInteraction(HttpServletRequest request, 
         HttpServletResponse response, NiggleConfig config)
 		throws IOException {
@@ -20,13 +19,24 @@ public class MyNiggleServletInteraction extends ServletInteraction {
 
 	/** The AddPerson handler, invoked for action="addperson" */
 	public void execAddPerson() throws IOException {
-		Record person = getNewRecord("person");
-		if (person == null) {
-			throw new IllegalArgumentException("can't get Record person");
+		// Get a database connection.
+		OreoDataSource people = getDataSource("peopledb");
+		if (people == null) {
+			throw new IllegalArgumentException("can't get DataSource peopledb");
 		}
+		// Create an empty Record of type "people"
+		Record person = getNewRecord("people");
+		if (person == null) {
+			throw new IllegalArgumentException("can't get Record people");
+		}
+
+		// Fill in its fields from the HTML form
 		fillInFields(person);
-		OreoDataSource people = getDataSource("people");
+
+		// Plonk it into the database
 		people.insert(person);
+
+		// Acknowledge addition to DB back to the user.
 		page = getPage("thanks.nhtml");
 		page.expose("inserted_ok", true);
 		page.expose("person", person);
