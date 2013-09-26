d2cad3866c0f71a6e1a5894815093239860fd4d8
Ian Darwin <ian@darwinsys.com>
2013-09-26 02:38:07 +0000

    Initial revision

---
+++ jwf-datamodel/src/main/java/com/darwinsys/jwf/model/Person.java
@@ -0,0 +1,20 @@
+package com.darwinsys.jwf.model;
+
+/**
+ * First version of a reusable Person entity
+ * for frameworks that can use POJO entities
+ * and/or JPA entities.
+ */
+public class Person {
+
+    public Long id;
+    public String firstname;
+    public String lastname;
+    public String email;
+    public String address1;
+    public String address2;
+    public String city;
+    public String province;
+    public String postcode;
+    public String country;
+}
