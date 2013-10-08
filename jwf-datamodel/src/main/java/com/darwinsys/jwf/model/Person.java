82a096ee88bb716db3545271ede43a3f4f26f8e1
Ian Darwin <ian@darwinsys.com>
2013-10-08 16:07:45 -0400

    Flesh out the spring+jsf example to use facelets and semblance of completeness; add JPA to model. Eclipse files.

--- jwf-datamodel/src/main/java/com/darwinsys/jwf/model/Person.java
+++ jwf-datamodel/src/main/java/com/darwinsys/jwf/model/Person.java
@@ -1,20 +1,93 @@
 package com.darwinsys.jwf.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Transient;
+
 /**
  * First version of a reusable Person entity
  * for frameworks that can use POJO entities
  * and/or JPA entities.
  */
+@Entity
 public class Person {
 
     public Long id;
-    public String firstname;
-    public String lastname;
+    public String firstName;
+    public String lastName;
     public String email;
     public String address1;
     public String address2;
     public String city;
     public String province;
-    public String postcode;
+    public String postCode;
     public String country;
+    
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	@Transient
+	public String getName() {
+		return getFirstName() + " " + getLastName();
+	}
 }
