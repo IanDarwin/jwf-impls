762097ebdbc2c5148edfd90d49dae2dd552ae053
Ian Darwin <ian@darwinsys.com>
2013-10-08 21:17:45 -0400

    Add an in-memory implementation.

---
+++ jwf-datamodel/src/main/java/com/darwinsys/jwf/model/PersonDaoMemory.java
@@ -0,0 +1,29 @@
+package com.darwinsys.jwf.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** 
+ * A completely toy DAO implementation: in-memory only!
+ * @author Ian Darwin
+ */
+public class PersonDaoMemory implements PersonDao {
+    
+    List<Person> frontingStore = new ArrayList<Person>(100);
+    
+    public PersonDao() {
+        // empty
+    }
+    
+    @Override
+    public boolean insert(Person p) {
+        boolean ok = frontingStore.add(p);
+        // XXX should save to a backing store too!
+        return ok;
+    }
+
+    @Override
+    public List findAll() {
+        return frontingStore;
+    }
+}
