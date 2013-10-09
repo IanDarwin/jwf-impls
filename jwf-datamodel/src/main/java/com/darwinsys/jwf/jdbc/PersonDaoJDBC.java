f038d7f06166343b37bc10c2ff36cea80537f57d
Ian Darwin <ian@darwinsys.com>
2013-10-08 20:33:50 -0400

    Start of a set of DAOs.

---
+++ jwf-datamodel/src/main/java/com/darwinsys/jwf/jdbc/PersonDaoJDBC.java
@@ -0,0 +1,108 @@
+package com.darwinsys.jwf.jdbc;
+
+import java.io.*;
+import java.sql.*;
+import javax.sql.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.darwinsys.jwf.model.Person;
+
+/* Data Accessor for "Person" in the JavaWebFraemwork.
+ * @author Ian F. Darwin
+ * $Id: PersonDAO.java,v 1.3 2003/07/07 01:32:45 ian Exp $
+ */
+
+public class PersonDAO {
+
+	private DataSource ds;
+	
+	private static final String fields =
+		"firstName, lastName, email, address1, address2," +
+			"city, province, postcode, country";
+
+	/** Constructor - just lookup DataSource */
+	public PersonDAO() throws NamingException {
+		Context ctx = new InitialContext();
+
+		String dsn = "java:comp/env/jdbc/javawebframeworks";
+		System.out.println("Looking up " + dsn);
+		Object o = ctx.lookup(dsn);
+		ds = (javax.sql.DataSource)o;
+		System.out.println("DataSource is " + ds);
+	}
+
+	/** insert - insert a Person object into the database. */
+	public boolean insert(Person person) {
+		try {
+			Connection con = ds.getConnection();
+
+			// Use a "throw-away" PreparedStatement - best practice - to
+			// avoid SQL insertion attacks common when string-concat used.
+			PreparedStatement st = con.prepareStatement(
+				"insert into people (" + fields + ")" +
+				"values(?,?,?,?,?,?,?,?,?)");
+
+			if (!person.isValid()) {
+				throw new IllegalArgumentException(
+					person.getErrors().toString());
+			}
+
+			int ret = 0, i = 1;
+
+			st.setString(i++, person.getFirstName());
+			st.setString(i++, person.getLastName());
+			st.setString(i++, person.getEmail());
+			st.setString(i++, person.getAddress1());
+			st.setString(i++, person.getAddress2());
+			st.setString(i++, person.getCity());
+			st.setString(i++, person.getProvince());
+			st.setString(i++, person.getPostcode());
+			st.setString(i++, person.getCountry());
+
+			ret = st.executeUpdate();
+
+			st.close();
+				
+			con.close();	// put back into connection pool
+
+			return (ret == 1);
+
+		} catch (SQLException ex) {
+			throw new IllegalStateException(ex.toString());
+		}
+	}
+
+	public List findAll() {
+		List all = new ArrayList();
+		try {
+			Connection c = ds.getConnection();
+			Statement s = c.createStatement();
+			ResultSet rs = s.executeQuery(
+				"select " + fields + " from people");
+			while (rs.next()) {
+				all.add(getPerson(rs));
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return all;
+	}
+
+	private Person getPerson(ResultSet rs) throws SQLException {
+		Person p = new Person();
+		p.setFirstName(rs.getString("FirstName"));
+		p.setLastName(rs.getString("LastName"));
+		p.setEmail(rs.getString("email"));
+		p.setAddress1(rs.getString("Address1"));
+		p.setAddress2(rs.getString("Address2"));
+		p.setCity(rs.getString("city"));
+		p.setProvince(rs.getString("province"));
+		p.setPostcode(rs.getString("postcode"));
+		p.setCountry(rs.getString("Country"));
+		return p;
+	}
+}
