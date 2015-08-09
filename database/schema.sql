011c385428817b3886d9e97254db81454da30f5b
Ian Darwin <ian@darwinsys.com>
2015-08-09 07:58:36 -0400

    Drop in an old version of a database schema; not used by in-memory dao.

---
+++ database/schema.sql
@@ -0,0 +1,12 @@
+create table people (
+        id integer primary key,
+        firstname varchar(40),
+        lastname varchar(40),
+        email varchar(60),
+        address1 varchar(40),
+        address2 varchar(40),
+        city varchar(40),
+        province varchar(30),
+        postcode varchar(10),
+        country varchar(30) 
+);
